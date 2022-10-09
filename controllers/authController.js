2ef10de38c7788ea2d74067d7a03efd3c2fe0da3
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-10-09 20:19:59 +0530

    grgrdg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes";
 
+class CustomAPIError extends Error{
+    constructor(message){
+        super(message)
+    }
+}
+
+class BadRequestError extends CustomAPIError{
+    constructor(message){
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
 const register= async (req,res)=>{
     const {name,email,password} = req.body;
     if(!name || !email || !password){
-        throw new Error('please provide all values')
+        throw new BadRequestError('please provide all values')
     }
 
     const user = await User.create({name,email,password});
